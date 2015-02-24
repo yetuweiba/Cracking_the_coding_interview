38c04a19e84e42301c8863e92a146cf73f6d018d
yetuweiba <niukaifang@gmail.com>
2015-02-25 06:08:16 +0800

    修改提交错误的第二章第五题

--- c++/Chapter_Two/Q2_5.cpp
+++ c++/Chapter_Two/Q2_5.cpp
@@ -1,69 +1,112 @@
-#include <iostream>
+// Given a circular linked list, implement an algorithm which returns node at the beginning of the loop.
+// DEFINITION
 
-const int STACK_LEN = 10;
+// Circular linked list: A (corrupt) linked list in which a node’s next pointer points to an earlier node, so as to make a loop in the linked list.
 
-class Stack
+// EXAMPLE
+
+// Input: A -> B -> C -> D -> E -> C [the same C as earlier]
+
+// Output: C
+
+// 即寻找链表中是否有环,如果有的话，返回环的节点。
+
+// 两种思路：
+// 1. 使用一个map来存储所有节点的地址，之后对比每个节点的地址，看是否有相同地址，如果有，则说明
+// 链表中有环，时间复杂度为O(nlogn).
+// 2. 使用两个指针，一个快指针，一个慢指针，当快指针赶上慢指针的时候，说明链表中存在环
+#include <stdio.h>
+
+struct Node
 {
-public:
-    Stack()
-        : m_nTopPoint(-1)
-    {
-        // do nothing
-    }
-    void Pop()
-    {
-        if (m_nTopPoint == -1)
-        {
-            return;
-        }
-        data[m_nTopPoint] = 0;
-        m_nTopPoint = m_nTopPoint - 1;
-    }
-
-    int Push(int a)
-    {
-        // 栈已满
-        if (m_nTopPoint == (STACK_LEN - 1))
-        {
-            return 1;
-        }
-
-        data[++m_nTopPoint] = a;
-        return 0;
-    }
-
-    int Top(int &a)
-    {
-        if (m_nTopPoint == (STACK_LEN - 1))
-        {
-            return 1;
-        }
-
-        a = data[m_nTopPoint];
-        return 0;
-    }
-
-    void PrintData()
-    {
-        for (int i = 0; i <= m_nTopPoint; ++i)
-        {
-            std::cout << data[i] << std::endl;
-        }
-    }
-private:
-    int m_nTopPoint;
-    int data[STACK_LEN];
+	int data;
+	Node *next;
+	Node()
+		:data(-1)
+		,next(NULL)
+	{
+		// empty
+	}
 };
+
+Node* init_list(int data[], const int len, const int m)
+{
+	Node* head = NULL;
+
+	if (!data || len <= 0)
+	{
+		return head;
+	}
+
+	Node* temp = new Node();
+	Node* loop_node = NULL;
+	temp->data = data[0];
+	head = temp;
+	for (int i = 1; i < len; ++i)
+	{
+		Node *temp1 = new Node();
+		temp->next = temp1;
+		temp1->data = data[i];
+		temp = temp->next;
+		if (i == m - 1)
+		{
+			loop_node = temp1;
+		}
+	}
+
+	temp->next = loop_node;
+
+	return head;
+}
+// 
+Node* get_loop_element(Node* head)
+{
+	Node *pResult = NULL;;
+
+	if (!head || !head->next)
+	{
+		return pResult;
+	}
+
+	Node *temp1 = head;
+	Node *temp2 = head;
+	
+	// 此循环是证明链表存在环
+	while(temp2 && temp2->next)
+	{
+		temp1 = temp1->next;
+		temp2 = temp2->next->next;
+		if (temp1 == temp2)
+		{
+			break;
+		}
+	}
+
+	// 到队尾，说明没有环
+	if (!temp2 || !temp2->next)
+	{
+		return pResult;
+	}
+
+	temp1 = head;
+	while(temp1 != temp2)
+	{
+		temp1 = temp1->next;
+		temp2 = temp2->next;
+	}
+	pResult = temp1;
+	return pResult;
+}
 
 int main()
 {
-    Stack st;
-    for (int i = 0; i < 11; ++i)
-    {
-        st.Push(i);
-    }
-
-    st.PrintData();
-
-    return 0;
+	// 目前申请的内存暂无法释放，因为链表中存在环
+	// 只有等到程序退出去后由系统销毁
+	int a[] = {
+		3, 6, 3, 5 
+	};
+	Node *head1 = init_list(a, 4, 2);
+	Node *loop_node = get_loop_element(head1);
+	printf("the loop data is: %d", loop_node->data);
+	return 0;
 }
